cb8fd0fcf0e299238ae967775afcf1cb4a152303
ulrich <git@myvdr.de>
2016-05-08 21:42:26 +0200

    Update index.js

--- doc-generator/index.js
+++ doc-generator/index.js
@@ -9,10 +9,29 @@ var content = fs.readFileSync('./data/powersocket.csv').toString();
 var input = content.toString();
 var records = parse(input, {columns: true, trim:true});
 
-var source = fs.readFileSync('./template.hbs').toString();
+var ergebnis = {};
+var ergebnis2 = {};
+for(var i=0;i<records.length;i++){
+	var sicherung = parseInt(records[i].Sicherung.match(/\d+/g))
 
+	if(records[i].Sicherung.match('HUV'))
+	{
+		if (!ergebnis[sicherung])
+			ergebnis[sicherung] = [];
+		ergebnis[sicherung].push(records[i]['Steckdosenbezeichnung']);
+		if (!ergebnis2[sicherung])
+			ergebnis2[sicherung] = [];
+		ergebnis2[sicherung].push(records[i]['Bezeichnung']);
+	}
+}
+
+var socket_list = require('./lib/socket_list');
+
+var source = fs.readFileSync('./template.hbs').toString();
 var template = Handlebars.compile(source);
 var data = [];
+
+var cleanup = require('./lib/cleanup');
 
 for(var i=0;i<json_data.huv_left.length;i++){
 	var obj = json_data.huv_left[i];
@@ -46,10 +65,21 @@ for(var i=0;i<json_data.huv_left.length;i++){
 			if(obj.stk2[j].hasOwnProperty("desc"))	
 				desclist.push(obj.stk2[j].desc);
 		}
-		var cleanup = require('./lib/cleanup')
-		obj.room = cleanup(roomlist).join(', ');
-		obj.name = cleanup(desclist).join();
 	}
+
+	if (obj.new_feature && ergebnis2[ obj.nr ]) {
+		desclist = desclist.concat(cleanup(ergebnis2[ obj.nr ]));
+		obj.socket_list = true;
+	}
+
+	var desc_socket_list = [];
+	if (obj.socket_list && ergebnis2[ obj.nr ]) {
+		desc_socket_list = socket_list(ergebnis[ obj.nr ]);
+	}
+
+	obj.room = cleanup(roomlist).join(', ');
+	obj.name = cleanup(desclist).join(', ');
+	obj.socket_list = cleanup(desc_socket_list).join(', ');
 
 	if(!obj.room) obj.room = "-";
 	if(!obj.name) obj.name = "-";
@@ -58,6 +88,6 @@ for(var i=0;i<json_data.huv_left.length;i++){
 	data.push(obj);
 }
 
-var result = template({stk: json_data, path: __dirname,huv_left:data});
+var result = template({stk: json_data, path: __dirname,huv_left:data,date:Date()});
 
 wkhtmltopdf(null, result, 'export.pdf');
